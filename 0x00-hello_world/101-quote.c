7b202ae6e0e902a550ed9dba6869756c556274ae
Joel Mwala <joelmwala7@gmail.com>
2023-03-16 16:34:26 -0700

    correct the last one

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-/*
- *a standard error program 
+/**
+ *main - prints that exact string
+ *Return: 1 on success
  */
 
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\
-\" - Dora Korpar, 2015-10-19\n");
+char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"); 
+fprintf(stderr, text);
 return (1);
 }
