44ab100bb94448bc9f6525f5840e24878446f3d5
Joel Mwala <joelmwala7@gmail.com>
2023-03-16 16:41:20 -0700

    correct the last one

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
  *main - prints that exact string
  *Return: 1 on success
  */
-
 int main(void)
 {
-char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"); 
+char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 fprintf(stderr, text);
 return (1);
 }
