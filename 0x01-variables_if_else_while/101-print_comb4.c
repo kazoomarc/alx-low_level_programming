56db97c766652dac242bab5f989c01837263d5ed
Joel Mwala <joelmwala7@gmail.com>
2023-03-17 16:08:16 -0700

    combine 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ *main - combination of 3-digits
+ *Return: 0 on success
+ */
+int main(void)
+{
+int number1, number2, number3;
+int counter = 0;
+for (number1 = 0; number1 < 8; number1++)
+{
+for (number2 = number1 + 1; number2 < 9; number2++)
+{
+for (number3 = number2 + 1; number3 < 10; number3++)
+{
+putchar(number1 + '0');
+putchar(number2 + '0');
+putchar(number3 + '0');
+counter++;
+if (count < 120)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
