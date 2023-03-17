bfbfaba9fee74372798f825bb35c4426a6f51231
Joel Mwala <joelmwala7@gmail.com>
2023-03-17 16:15:17 -0700

    2 2digit number

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ *main - 2 digit combinations
+ *Return: (success)
+ */
+int main(void)
+{
+int number1, number2;
+for (number1 = 0; number1 <= 99; number1++)
+{
+for (number2 = number1; number2 <= 99; number2++)
+{
+if (number1 / 10 <= number2 / 10)
+{
+putchar(number1 / 10 + '0');
+putchar(number1 % 10 + '0');
+putchar(' ');
+putchar(number2 / 10 + '0');
+putchar(number2 % 10 + '0');
+if (number1 != 99 || number2 != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
