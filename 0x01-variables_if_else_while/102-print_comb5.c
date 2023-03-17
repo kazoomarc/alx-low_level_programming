9c517aac759795c7813cf0d919a1779f65aeb1f1
Joel Mwala <joelmwala7@gmail.com>
2023-03-17 16:23:40 -0700

    2 2digit number

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,7 +10,9 @@ for (number1 = 0; number1 <= 99; number1++)
 {
 for (number2 = number1; number2 <= 99; number2++)
 {
-if (number1 / 10 <= number2 / 10 && (number1/10 == 0 && number2 == 0 && (number2 / 10) == 0 && (number2 % 10) == 0))
+if (number1 / 10 <= number2 / 10 && \
+(number1 / 10 == 0 && number2 == 0 && \
+(number2 / 10) == 0 && (number2 % 10) == 0))
 {
 putchar(number1 / 10 + '0');
 putchar(number1 % 10 + '0');
