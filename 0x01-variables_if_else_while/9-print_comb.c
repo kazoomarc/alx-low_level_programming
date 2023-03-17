3353156ccfcbe4bef58fd3be0a8bfade914cb713
Joel Mwala <joelmwala7@gmail.com>
2023-03-17 15:50:26 -0700

    number commas list

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main - numbers less than 10 separated by commas
+*
+*Return: 0 on success
+*/
+int main(void)
+{
+int number;
+number = 0;
+while (number < 10)
+{
+putchar(number + '0');
+if (number < 9)
+{
+putchar(',');
+putchar(' ');
+}
+else
+putchar(' ');
+number++;
+}
+return (0);
+}
