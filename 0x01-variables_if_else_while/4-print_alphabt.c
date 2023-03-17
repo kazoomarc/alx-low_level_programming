20e5a7ae07e43dc900e66b28940bcb1cad4863bd
Joel Mwala <joelmwala7@gmail.com>
2023-03-17 14:54:12 -0700

    skip q and e letters alph

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+* main - program to print alphabet line by line
+*
+* Return: 0 (success)
+*/
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+if (c == 'q' || c == 'e')
+continue;
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
+
+
