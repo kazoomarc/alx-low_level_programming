60a6a59aa626bb70fe05d65db7ef25a2655bb988
Joel Mwala <joelmwala7@gmail.com>
2023-03-21 15:41:57 -0700

    11 again

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,9 +2,14 @@
 #include<stdio.h>
 #include<unistd.h>
 /**
+<<<<<<< HEAD
  *print_to_98 - prints to 98
  *print_number - it does the format printing
  *@num: has number to be checked
+=======
+ *print_sign - prints to 98
+ *@n: has number to be checked
+>>>>>>> parent of b4b8d0b... 11 commit
  *
  *Return: 0 (success)
  */
@@ -16,10 +21,14 @@ _putchar('-');
 num = -num;
 }
 
+<<<<<<< HEAD
 if (num / 10)
+=======
+if(num/10)
+>>>>>>> parent of b4b8d0b... 11 commit
 {
-print_number(num / 10);
-_putchar(num % 10 + '0');
+print_number(num/10);
+_putchar(num%10 + '0');
 }
 }
 
@@ -46,8 +55,7 @@ else
 while (start >= 98)
 {
 print_number(start);
-if (start != 98)
-{
+if (start != 98) {
 _putchar(',');
 _putchar(' ');
 }
