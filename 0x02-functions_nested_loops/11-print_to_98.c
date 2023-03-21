a474054626069e8597bd512efddfa9c46022827d
Joel Mwala <joelmwala7@gmail.com>
2023-03-21 15:21:14 -0700

    11 commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,13 +9,13 @@
  */
 void print_number(int num)
 {
-if(num < 0)
+if (num < 0)
 {
 _putchar('-');
 num = -num;
 }
 
-if(num / 10)
+if (num / 10)
 {
 print_number(num / 10);
 _putchar(num % 10 + '0');
