371304dfb3e164e7548e4f7f865397bd57552f3c
Joel Mwala <joelmwala7@gmail.com>
2023-03-21 14:44:23 -0700

    remive white trails in 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *_abs - returns absolute value of a number 
- *
- *@abs_num: takes variable to be operated by absolute function
- *
- *Return: 0 (success)
- */
+*_abs - returns absolute value of a number
+*
+*@abs_num: takes variable to be operated by absolute function
+*
+*Return: 0 (success)
+*/
 int _abs(int abs_num)
 {
 if (abs_num < 0)
