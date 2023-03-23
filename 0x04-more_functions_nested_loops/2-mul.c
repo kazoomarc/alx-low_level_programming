7432fc37c9c1258e8984974879567a5b95e86915
Joel Mwala <joelmwala7@gmail.com>
2023-03-23 14:45:31 -0700

    multiply a & b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *mul - multiply two ints a & b
+ *
+ * @a: intiger 1
+ * @b: intiger 2
+ * 
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+int result = a * b;
+return (result);
+}
