a9aced2ea4b6226bcc226343d9f621cd15d75e4c
Joel Mwala <joelmwala7@gmail.com>
2023-03-27 13:43:04 -0700

    switch a, b function

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - meaning
+ *@a: value 1 to be swapped to b
+ *@b: value 2 to be swaped to a
+ *Return: Nothing
+*/
+
+void swap_int(int *a, int *b)
+{
+int temp = *a;
+*a = *b;
+*b = tem
