8d90cbe2c2ff9e67f320c61be3825c0c337e1333
Joel Mwala <joelmwala7@gmail.com>
2023-03-29 11:55:18 -0700

    copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,17 +11,6 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
-for (i = 0; i < n && i != '\0'; i++)
-{
-dest[i] = src[i];
-}
-
-for (; i < n; i++)
-{
-dest[i] = '\0';
-}
-
-return (dest);
+return (strncpy(dest, src, n));
 }
 
