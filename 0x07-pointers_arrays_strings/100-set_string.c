c57727633b1824cb7093c9db251d73a8eebc2406
Joel Mwala <joelmwala7@gmail.com>
2023-04-03 16:58:16 -0700

    swap

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+
+#include "main.h"
+/**
+ * set_string - changes s to point to char to
+ * @s: points to to string
+ * @to: the pointer holding a string
+ * Return: (Nothing)
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
+
