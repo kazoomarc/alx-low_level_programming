c1ede0437359b8502cf712365b366416409f793a
Joel Mwala <joelmwala7@gmail.com>
2023-04-03 16:31:51 -0700

    edit long line

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strpbrk - locates the first occurrence in the string s of any of the bytes in the string accept.
+ * _strpbrk - locates the first occurrence in the string s.
  * @s: string to be searched for accept
  * @accept: strind required
  * Return: Always 0 (Success)
