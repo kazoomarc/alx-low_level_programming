7b8b00018245163f74510b659a13d32b57088e0c
Joel Mwala <joelmwala7@gmail.com>
2023-04-11 10:37:09 -0700

    arg counter

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - print argc values
+ * @argc: arguments counter
+ * @argv: arg array
+ *
+ * Return:0 (Success)
+ */
+
+int main(int argc, char* argv[]){
+(void) argv;
+printf("%d\n", argc - 1);
+return (0);
+}
+
