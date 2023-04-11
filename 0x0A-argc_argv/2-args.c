7cffbcf728963bd090ca275b4647dbc21c9858e4
Joel Mwala <joelmwala7@gmail.com>
2023-04-11 10:52:32 -0700

    submit 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print argc values
+ * @argc: arguments counter
+ * @argv: arg array
+ *
+ * Return:0 (Success)
+ */
+int main(int argc, char* argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+printf("%s\n", argv[i]);
+        
+return (0);
+}
