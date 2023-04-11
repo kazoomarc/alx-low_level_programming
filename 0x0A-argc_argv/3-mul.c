a5f88f07c0fede2c771e41768a0e50038ad49fee
Joel Mwala <joelmwala7@gmail.com>
2023-04-11 11:15:22 -0700

    multiply args

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,14 +10,17 @@
 
 int main(int argc, char *argv[])
 {
-if (argc == 3)
-{
-printf("%d", (atoi(argv[1]) * atoi(argv[2])));
-return (0);
-}
-else
+if (argc < 3 || argc > 3)
 {
 printf("Error\n");
 return (1);
 }
+}
+
+
+printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
+return (0);
+
+
+
 }
