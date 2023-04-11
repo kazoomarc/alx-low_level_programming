707b65b9bc87746717081a9f5d69fa7233eb2d21
Joel Mwala <joelmwala7@gmail.com>
2023-04-11 11:39:20 -0700

    commit on add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Print the name of the program
+ * @argc: argv count
+ * @argv: input args
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+int sum = 0;
+if (argc < 1)
+{
+printf("0");
+}
+else
+{
+int i;
+for (i = 1; i < argc; i++)
+{
+int n;
+if (sscanf(argv[i], "%d", &n) != 1)
+{
+printf("Error\n");
+return (1);
+break;
+}
+sum = sum + n;
+}
+if (i == argc)
+{
+printf("%d\n", sum);
+}
+}
+}
