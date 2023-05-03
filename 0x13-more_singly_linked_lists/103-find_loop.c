62f1d5f02744726a17348cd14343255f0cd95131
Joel Mwala <joelmwala7@gmail.com>
2023-05-03 12:38:01 -0700

    snake

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,22 +1,19 @@
 #include "lists.h"
 
-size_t looped_listint_count(listint_t *head);
-size_t free_listint_safe(listint_t **h);
-
 /**
- * looped_listint_count - Counts the number of unique nodes
- * in a looped linked list.
- * @head:-> head of the listint_t to check.
- * Return: the number of unique nodes in the list.
+ * find_listint_loop - Finds the loop contained in
+ * a linked list.
+ * @head: -> the head of the listint_t list.
+ *
+ * Return: the address of the node where the loop starts.
  */
-size_t looped_listint_count(listint_t *head)
+listint_t *find_listint_loop(listint_t *head)
 {
 listint_t *turtle, *snake;
-size_t nodes = 1;
 
 if (head == NULL || head->next == NULL)
 {
-return (0);
+return (NULL);
 }
 
 turtle = head->next;
@@ -27,65 +24,19 @@ while (snake)
 if (turtle == snake)
 {
 turtle = head;
+
 while (turtle != snake)
 {
-nodes++;
 turtle = turtle->next;
 snake = snake->next;
 }
 
-turtle = turtle->next;
-while (turtle != snake)
-{
-nodes++;
-turtle = turtle->next;
-}
-
-return (nodes);
+return (turtle);
 }
 
 turtle = turtle->next;
 snake = (snake->next)->next;
 }
 
-return (0);
-}
-
-/**
- * free_listint_safe - Frees a listint_t list safely 
- * @h: -> the head of the listint_t list.
- * Return:size of the list that was freed.
- */
-size_t free_listint_safe(listint_t **h)
-{
-listint_t *temp;
-size_t nodes, j;
-
-nodes = looped_listint_count(*h);
-
-if (nodes == 0)
-{
-for (; h != NULL && *h != NULL; nodes++)
-{
-temp = (*h)->next;
-free(*h);
-*h = temp;
-}
-}
-
-else
-{
-for (j = 0; j < nodes; j++)
-{
-temp = (*h)->next;
-free(*h);
-*h = temp;
-}
-
-*h = NULL;
-}
-
-h = NULL;
-
-return (nodes);
+return (NULL);
 }
