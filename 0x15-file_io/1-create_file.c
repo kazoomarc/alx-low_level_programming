7aaace37d5a549045e932715e63368146863bd7d
Joel Mwala <joelmwala7@gmail.com>
2023-05-08 14:53:07 -0700

    tired

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * create_file - makes a file
+ * @filename: -> file to create.
+ * @text_content:-> string to write to the file.
+ *
+ * Return: Fail (-1) | Otherwise (1)
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+int o, w, len = 0;
+
+if (filename == NULL)
+{
+return (-1);
+}
+
+if (text_content != NULL)
+{
+for (len = 0; text_content[len];)
+{
+len++;
+}
+}
+
+o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+w = write(o, text_content, len);
+
+if (o == -1 || w == -1)
+{
+return (-1);
+}
+
+close(o);
+
+return (1);
+}
